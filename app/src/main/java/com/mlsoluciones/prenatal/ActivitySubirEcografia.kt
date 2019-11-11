ca6f51bc7cc818fbf8cf671629823dd32152f667
Diego Molina <ingsistem1009@gmail.com>
2019-11-11 18:31:16 -0500

    proyecto prenatal

---
+++ app/src/main/java/com/mlsoluciones/prenatal/ActivitySubirEcografia.kt
@@ -0,0 +1,12 @@
+package com.mlsoluciones.prenatal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivitySubirEcografia : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_subir_ecografia)
+    }
+}
