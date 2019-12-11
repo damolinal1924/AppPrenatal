0b7010459a4a9e0c72b6773cf1d1af6dce672073
Diego Molina <ingsistem1009@gmail.com>
2019-12-11 00:42:04 -0500

    Botones Ecografias y pantallas de subir imagene

--- app/src/main/java/com/mlsoluciones/prenatal/RegistrateActivity.kt
+++ app/src/main/java/com/mlsoluciones/prenatal/RegistrateActivity.kt
@@ -2,6 +2,9 @@ package com.mlsoluciones.prenatal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.RadioButton
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class RegistrateActivity : AppCompatActivity() {
@@ -10,5 +13,24 @@ class RegistrateActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registrate)
 
+    }
+
+    fun seleccionarMadreRH(view: View) {
+        if (view is RadioButton) {
+            // Is the button now checked?
+            val checked = view.isChecked
+
+            // Check which radio button was clicked
+            when (view.getId()) {
+                R.id.rbMadreRHsi ->
+                    if (checked) {
+                        Toast.makeText(this@RegistrateActivity, "Se ha presionado que SI", Toast.LENGTH_LONG).show()
+                    }
+                R.id.rbMadreRHno ->
+                    if (checked) {
+                        Toast.makeText(this@RegistrateActivity, "Se ha presionado que No", Toast.LENGTH_LONG).show()
+                    }
+            }
+        }
     }
 }
